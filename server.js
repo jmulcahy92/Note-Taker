19969ca3f3bdacce37d38c7f314438e16d0529e2
jmulcahy92 <jacobkmulcahy@gmail.com>
2023-02-26 16:18:29 -0600

    quality comments

--- server.js
+++ server.js
@@ -1,25 +1,35 @@
+// import express, path, and ./routes/index.js
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
 
-const PORT = 3001;
+// set PORT based on environment or default to 3001
+const PORT = process.env.PORT || 3001;
 
+// initialize our app variable by setting it to the value of express()
 const app = express();
 
+// middleware for parsing json and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// look to ./routes/index.js for api requests
 app.use('/api', api);
 
+// 'public' folder contains front-end
 app.use(express.static('public'));
 
+// GET homepage (technically unnecessary thanks to above line and naming appropriate file index.html)
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// GET notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// listen on environment-determined port or port 3001 by default
 app.listen(PORT, () => {
     console.log(`Example app listening at http://localhost:${PORT}`);
 });
